1211398cbaf98068f1d8c17c40f1eb0f5cc377c6
sbacs <dalcaldoalfreddodigiorno@gmail.com>
2025-03-03 18:48:44 +0100

    58785765

--- main.py
+++ main.py
@@ -24,16 +24,46 @@ cursor = connection.cursor()
 cursor.execute(Utils.getQuery(0)) 
 res = cursor.fetchall()
 
-field_names = [i[0] for i in cursor.description]
-
-my_dict = [dict(zip(field_names, x)) for x in res]
+print(res)
 
 
-jsone = json.dumps(my_dict)
+firstRow = res[0]
+day = firstRow[0].split(" ")[0]
 
+
+
+expenseCenter = str(firstRow[1]) + "-" + str(firstRow[2])
+start = 540 
+end = 555
+tickets = firstRow[4]
+
+sales = 0
+for row in res:
+    sales += row[3]
+    
+
+Steps = [{"start": start, "end": end, "sales": sales, "tickets":tickets}]
+expenseCenters = [{"expenseCenter": expenseCenter ,"steps": Steps}]
+
+days = [{"day": day, "expenseCenters" : expenseCenters}]
+Final = {"days" : days, "start":start, "end":end}
+
+FinalJson = json.dumps(Final)
 
 with open("Data.json", "w") as file:
-    file.write(jsone)
+    file.write(FinalJson)
+
+
+#field_names = [i[0] for i in cursor.description]
+
+#my_dict = [dict(zip(field_names, x)) for x in res]
+#
+#
+#jsone = json.dumps(my_dict)
+#
+#
+#with open("Data.json", "w") as file:
+#    file.write(jsone)
 
 
 
